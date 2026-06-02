a7336108fdcff0f343a1345d3b451ad9a511e287
pranav718 <raypranav718@gmail.com>
2026-06-03 00:44:52 +0530

    network upload and download speed counters

---
+++ internal/stats/network.go
@@ -0,0 +1,61 @@
+package stats
+
+import (
+	"time"
+
+	"github.com/shirou/gopsutil/v4/net"
+)
+
+type NetworkStats struct {
+	BytesInPerSec  float64
+	BytesOutPerSec float64
+}
+
+type NetworkTracker struct {
+	lastIn  uint64
+	lastOut uint64
+	lastAt  time.Time
+	ready   bool
+}
+
+func FetchNetwork(tracker *NetworkTracker) (NetworkStats, error) {
+	counters, err := net.IOCounters(false)
+	if err != nil {
+		return NetworkStats{}, err
+	}
+	if len(counters) == 0 {
+		return NetworkStats{}, nil
+	}
+
+	now := time.Now()
+	currentIn := counters[0].BytesRecv
+	currentOut := counters[0].BytesSent
+
+	if tracker == nil {
+		return NetworkStats{}, nil
+	}
+
+	if !tracker.ready {
+		tracker.lastIn = currentIn
+		tracker.lastOut = currentOut
+		tracker.lastAt = now
+		tracker.ready = true
+		return NetworkStats{}, nil
+	}
+
+	elapsed := now.Sub(tracker.lastAt).Seconds()
+	if elapsed <= 0 {
+		elapsed = 1
+	}
+
+	stats := NetworkStats{
+		BytesInPerSec:  float64(currentIn-tracker.lastIn) / elapsed,
+		BytesOutPerSec: float64(currentOut-tracker.lastOut) / elapsed,
+	}
+
+	tracker.lastIn = currentIn
+	tracker.lastOut = currentOut
+	tracker.lastAt = now
+
+	return stats, nil
+}
